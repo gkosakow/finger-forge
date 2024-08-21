0db7f4b70c262a35cc96a16ac0f2489b1ea491e6
Greg Kosakowski <gkosakow@umich.edu>
2024-08-21 00:06:31 -0400

    putting Timer component in Home

---
+++ src/pages/Home.tsx
@@ -0,0 +1,12 @@
+import '../styles/Home.css';
+import Timer from '../components/Timer';
+
+function Home() {
+  return (
+    <div className='container'>
+      <Timer />
+    </div>
+  )
+}
+
+export default Home;
